223c45c79a9aa433c84d387aaa808ad30e5f56c8
Derek G <45305531+dGrowl@users.noreply.github.com>
2025-01-28 18:09:46 -0800

    Add linting and formatting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import Fastify from 'fastify'
+import Fastify from "fastify"
 
 const server = Fastify({ logger: true })
 
-server.get('/', async (_request, reply) => {
-	reply.send({ greeting: 'Hello, Fetch!' })
+server.get("/", async (_request, reply) => {
+	reply.send({ greeting: "Hello, Fetch!" })
 })
 
 try {
 	await server.listen({ port: 3000 })
-}
-catch (error) {
+} catch (error) {
 	server.log.error(error)
 	process.exit(1)
 }
