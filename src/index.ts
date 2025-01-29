462c54d29cfe0bd58d2e3b12dce28b61184eea95
Derek G <45305531+dGrowl@users.noreply.github.com>
2025-01-28 17:27:35 -0800

    Add server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import { greet } from './helpers.js'
+import Fastify from 'fastify'
 
-greet('Fetch')
+const server = Fastify({ logger: true })
+
+server.get('/', async (_request, reply) => {
+	reply.send({ greeting: 'Hello, Fetch!' })
+})
+
+try {
+	await server.listen({ port: 3000 })
+}
+catch (error) {
+	server.log.error(error)
+	process.exit(1)
+}
